ee6e696fb49a278d73d3cd901eb60352716aabd3
L30n95 <leon.hennessy@students.ittralee.ie>
2018-10-19 14:31:19 +0100

    Finished Ex 2

--- Lab 4 - OOP/src/AccountDriver.java
+++ Lab 4 - OOP/src/AccountDriver.java
@@ -4,7 +4,19 @@ public class AccountDriver {
 
     public static void main(String[] args) {
 
-        SavingsAccount account = new SavingsAccount();
+
+
+        SavingsAccount account = new SavingsAccount("Leon Hennessy",123456789,15241.89);
+
+        JOptionPane.showMessageDialog(null,account.toString());
+
+        JOptionPane.showMessageDialog(null,String.format("Tax due on amount is €%.2f",account.calcTax()));
+
+        account.lodge(500.99);
+
+        JOptionPane.showMessageDialog(null,account.toString());
+
+        account.withdraw(499.25);
 
         JOptionPane.showMessageDialog(null,account.toString());
 
